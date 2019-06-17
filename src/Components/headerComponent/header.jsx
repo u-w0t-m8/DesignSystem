49e02e41498eee32631d139ee6dfe53cbe3f9be2
jess <jessicatsang32@gmail.com>
2019-06-17 10:01:57 +1000

    Clean up, changed class to classname on header and footer

--- src/Components/headerComponent/header.jsx
+++ src/Components/headerComponent/header.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import { Link } from "react-router";
 
-// import { BrowserRouter as Link } from "react-router-dom";
+import { BrowserRouter as Link } from "react-router-dom";
 
 //components
 
