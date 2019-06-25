50c218c354a10fc74ecf9825ad1d75cfce9957ee
jess <jessicatsang32@gmail.com>
2019-06-25 14:24:55 +1000

    cleaned up code for colour palette dynamic rendering

--- src/Components/pages/colourPage.jsx
+++ src/Components/pages/colourPage.jsx
@@ -22,7 +22,7 @@ class ColourPage extends Component {
     );
   }
 
-  createTable = () => {
+  createColourPalette = () => {
     let labels = [
       "Dark blue",
       "Primary Blue",
@@ -83,20 +83,25 @@ class ColourPage extends Component {
       "#2e9ac5",
       "#a64d79"
     ];
-    let tables = [];
+    let palette = [];
+    var textColour = "#fff";
 
-    // Outer loop to create parent
     for (let i = 0; i < labels.length; i++) {
-      //Create the parent and add the children
-      tables.push(
+      if (hexCodes[i].includes("F") && !labels[i].includes("Error Red")) {
+        textColour = "#000";
+      } else {
+        textColour = "#fff";
+      }
+      palette.push(
         <ColourPalette
           label={labels[i]}
           scss={scssCodes[i]}
           hexCode={hexCodes[i]}
+          textColour={textColour}
         />
       );
     }
-    return tables;
+    return palette;
   };
 
   render() {
@@ -134,107 +139,8 @@ class ColourPage extends Component {
           <div className="content" />
 
           {/* dynamically rendering from array */}
-          {/* <div className="row test">{this.createTable()}</div> */}
-
           <div className="col-md-12">
-            <div className="row">
-              {this.renderPalette(
-                "Dark Blue",
-                "$darkPrimary",
-                "#154075",
-                "#fff"
-              )}
-              {this.renderPalette(
-                "Primary Blue",
-                "$primaryColour",
-                "#2A81EA",
-                "#fff"
-              )}
-              {this.renderPalette(
-                "Light Blue",
-                "$lightPrimary",
-                "#94C0F4",
-                "#000"
-              )}
-              {this.renderPalette(
-                "Pastel Blue",
-                "$pastelPrimary",
-                "#E3F2FF",
-                "#000"
-              )}
-            </div>
-
-            <div className="row">
-              {this.renderPalette(
-                "Dark Orange",
-                "$darkSecondary",
-                "#C14310",
-                "#fff"
-              )}
-              {this.renderPalette(
-                "Orange",
-                "$SecondaryColour",
-                "#EE6831",
-                "#fff"
-              )}
-              {this.renderPalette(
-                "Light Orange",
-                "$lightSecondary",
-                "#F49D79",
-                "#000"
-              )}
-              {this.renderPalette(
-                "Pastel Orange",
-                "$pastelSecondary",
-                "#FAD2C2",
-                "#000"
-              )}
-            </div>
-
-            <div className="row">
-              {this.renderPalette("Error red", "$error", "#EF3C3C", "#fff")}
-              {this.renderPalette("Warning", "$warning", "#F7C85F", "#000")}
-              {this.renderPalette(
-                "Success Green",
-                "$success",
-                "#3DBA50",
-                "#fff"
-              )}
-            </div>
-
-            <div className="row">
-              {this.renderPalette(
-                "Error Background",
-                "$errorBG",
-                "#FFCECE",
-                "#000"
-              )}
-
-              {this.renderPalette(
-                "Warning Background",
-                "$warningBG",
-                "#FFF4DD",
-                "#000"
-              )}
-              {this.renderPalette(
-                "Success Background",
-                "$successBG",
-                "#C0F5C8",
-                "#000"
-              )}
-            </div>
-
-            <div className="row">
-              {this.renderPalette("purple", "$purple", "#8472bb", "#fff")}
-              {this.renderPalette(
-                "Secondary blue",
-                "$blueMid",
-                "#3a6493",
-                "#fff"
-              )}
-              {this.renderPalette("Teal", "$teal", "#2e9ac5", "#fff")}
-              {this.renderPalette("Pinkle", "$pinkle", "#a64d79", "#fff")}
-            </div>
+            <div className="row">{this.createColourPalette()}</div>
           </div>
         </div>
       </div>
