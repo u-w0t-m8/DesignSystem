2898090a6930a59ccc3dc0a9aa9b11c1d9e8d379
jess <jessicatsang32@gmail.com>
2019-07-05 14:32:26 +1000

    removed warning changed footer link hover colour

--- src/Components/pages/homePage.jsx
+++ src/Components/pages/homePage.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 //components
-import FormComponent from "../blockComponents/formComponent";
+//import FormComponent from "../blockComponents/formComponent";
 
 class Homepage extends Component {
   //state = {  }
