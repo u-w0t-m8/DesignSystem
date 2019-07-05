9bbc3728d2811e2ba3e0ae89d7f2d2f1c7706ac0
jess <jessicatsang32@gmail.com>
2019-07-05 13:04:30 +1000

    Converted classes fo functional components

--- src/Components/blockComponents/bpTableCell.jsx
+++ src/Components/blockComponents/bpTableCell.jsx
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
-class BPTableCell extends Component {
-  render() {
-    return (
-      <tr>
-        <td>{this.props.bpRange}</td>
-        <td>{this.props.portrait}</td>
-        <td>{this.props.landscape}</td>
-        <td> {this.props.window}</td>
-        <td>{this.props.columns}</td>
-        <td>{this.props.margins}</td>
-      </tr>
-    );
-  }
-}
+const BPTableCell = props => {
+  return (
+    <tr>
+      <td>{this.props.bpRange}</td>
+      <td>{this.props.portrait}</td>
+      <td>{this.props.landscape}</td>
+      <td> {this.props.window}</td>
+      <td>{this.props.columns}</td>
+      <td>{this.props.margins}</td>
+    </tr>
+  );
+};
 
 export default BPTableCell;
--- src/Components/blockComponents/breakpointLayout.jsx
+++ src/Components/blockComponents/breakpointLayout.jsx
@@ -1,118 +1,116 @@
-import React, { Component } from "react";
+import React from "react";
 import BPTableRow from "../blockComponents/bpTableCell";
 
-class BreakpointTable extends Component {
-  render() {
-    return (
-      <table className="breakpoint-table col-md-12">
-        <thead>
-          <tr>
-            <th>Breakpoint Range (dp)</th>
-            <th>Portrait</th>
-            <th>Landscape</th>
-            <th>Window</th>
-            <th>Columns</th>
-            <th>Margins/Gutters*</th>
-          </tr>
-        </thead>
-        <tbody>
-          <BPTableRow
-            bpRange={"0 - 359"}
-            portrait={"Small Handset"}
-            window={"xsmall"}
-            columns={"4"}
-            margins={"16"}
-          />
-          <BPTableRow
-            bpRange={"360 - 399"}
-            portrait={"Medium Handset"}
-            window={"xsmall"}
-            columns={"4"}
-            margins={"16"}
-          />
-          <BPTableRow
-            bpRange={"400 – 479"}
-            portrait={"large handset"}
-            window={"xsmall"}
-            columns={"4"}
-            margins={"16"}
-          />
-          <BPTableRow
-            bpRange={"480 – 599"}
-            portrait={"large handset"}
-            landscape={"small handset"}
-            window={"xsmall"}
-            columns={"4"}
-            margins={"16"}
-          />
-          <BPTableRow
-            bpRange={"600 – 719"}
-            portrait={"small tablet"}
-            landscape={"medium handset"}
-            window={"small"}
-            columns={"8"}
-            margins={"16"}
-          />
-          <BPTableRow
-            bpRange={"720-839"}
-            portrait={"large tablet"}
-            landscape={"large handset"}
-            window={"small"}
-            columns={"8"}
-            margins={"24"}
-          />
-          <BPTableRow
-            bpRange={"840-959"}
-            portrait={"large tablet"}
-            landscape={"large handset"}
-            window={"small"}
-            columns={"12"}
-            margins={"24"}
-          />
-          <BPTableRow
-            bpRange={"960-1023"}
-            landscape={"small tablet"}
-            window={"small"}
-            columns={"12"}
-            margins={"24"}
-          />
-          <BPTableRow
-            bpRange={"1024-1279"}
-            landscape={"large tablet"}
-            window={"medium"}
-            columns={"12"}
-            margins={"24"}
-          />
-          <BPTableRow
-            bpRange={"1280-1439"}
-            landscape={"large tablet"}
-            window={"medium"}
-            columns={"12"}
-            margins={"24"}
-          />
+const BreakpointTable = props => {
+  return (
+    <table className="breakpoint-table col-md-12">
+      <thead>
+        <tr>
+          <th>Breakpoint Range (dp)</th>
+          <th>Portrait</th>
+          <th>Landscape</th>
+          <th>Window</th>
+          <th>Columns</th>
+          <th>Margins/Gutters*</th>
+        </tr>
+      </thead>
+      <tbody>
+        <BPTableRow
+          bpRange={"0 - 359"}
+          portrait={"Small Handset"}
+          window={"xsmall"}
+          columns={"4"}
+          margins={"16"}
+        />
+        <BPTableRow
+          bpRange={"360 - 399"}
+          portrait={"Medium Handset"}
+          window={"xsmall"}
+          columns={"4"}
+          margins={"16"}
+        />
+        <BPTableRow
+          bpRange={"400 – 479"}
+          portrait={"large handset"}
+          window={"xsmall"}
+          columns={"4"}
+          margins={"16"}
+        />
+        <BPTableRow
+          bpRange={"480 – 599"}
+          portrait={"large handset"}
+          landscape={"small handset"}
+          window={"xsmall"}
+          columns={"4"}
+          margins={"16"}
+        />
+        <BPTableRow
+          bpRange={"600 – 719"}
+          portrait={"small tablet"}
+          landscape={"medium handset"}
+          window={"small"}
+          columns={"8"}
+          margins={"16"}
+        />
+        <BPTableRow
+          bpRange={"720-839"}
+          portrait={"large tablet"}
+          landscape={"large handset"}
+          window={"small"}
+          columns={"8"}
+          margins={"24"}
+        />
+        <BPTableRow
+          bpRange={"840-959"}
+          portrait={"large tablet"}
+          landscape={"large handset"}
+          window={"small"}
+          columns={"12"}
+          margins={"24"}
+        />
+        <BPTableRow
+          bpRange={"960-1023"}
+          landscape={"small tablet"}
+          window={"small"}
+          columns={"12"}
+          margins={"24"}
+        />
+        <BPTableRow
+          bpRange={"1024-1279"}
+          landscape={"large tablet"}
+          window={"medium"}
+          columns={"12"}
+          margins={"24"}
+        />
+        <BPTableRow
+          bpRange={"1280-1439"}
+          landscape={"large tablet"}
+          window={"medium"}
+          columns={"12"}
+          margins={"24"}
+        />
 
-          <BPTableRow
-            bpRange={"1440-1599"}
-            window={"large"}
-            columns={"12"}
-            margins={"24"}
-          />
-          <BPTableRow
-            bpRange={"1600-1919"}
-            window={"large"}
-            columns={"12"}
-            margins={"24"}
-          />
-          <BPTableRow
-            bpRange={"1920+"}
-            window={"xlarge"}
-            columns={"12"}
-            margins={"24"}
-          />
-        </tbody>
-      </table>
-    );
-  }
-}
+        <BPTableRow
+          bpRange={"1440-1599"}
+          window={"large"}
+          columns={"12"}
+          margins={"24"}
+        />
+        <BPTableRow
+          bpRange={"1600-1919"}
+          window={"large"}
+          columns={"12"}
+          margins={"24"}
+        />
+        <BPTableRow
+          bpRange={"1920+"}
+          window={"xlarge"}
+          columns={"12"}
+          margins={"24"}
+        />
+      </tbody>
+    </table>
+  );
+};
 
 export default BreakpointTable;
