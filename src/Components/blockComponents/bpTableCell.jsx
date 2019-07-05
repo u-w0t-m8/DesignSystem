f25723f313456f4a33c82b57ba53f2c71db5a8b6
jess <jessicatsang32@gmail.com>
2019-07-05 14:14:34 +1000

    Fixed error on layout page

--- src/Components/blockComponents/bpTableCell.jsx
+++ src/Components/blockComponents/bpTableCell.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 
-const BPTableCell = props => {
-  return (
-    <tr>
-      <td>{this.props.bpRange}</td>
-      <td>{this.props.portrait}</td>
-      <td>{this.props.landscape}</td>
-      <td> {this.props.window}</td>
-      <td>{this.props.columns}</td>
-      <td>{this.props.margins}</td>
-    </tr>
-  );
-};
+class BPTableCell extends Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.bpRange}</td>
+        <td>{this.props.portrait}</td>
+        <td>{this.props.landscape}</td>
+        <td> {this.props.window}</td>
+        <td>{this.props.columns}</td>
+        <td>{this.props.margins}</td>
+      </tr>
+    );
+  }
+}
 
 export default BPTableCell;
